6c2552ae10020ebd7edb018783ae51225bfb4cec
fadarash <fadarash@gmail.com>
2022-04-27 15:53:24 +0430

    add all Region option

--- src/components/SearchItem.jsx
+++ src/components/SearchItem.jsx
@@ -17,19 +17,19 @@ export const SearchItem = ({ handleRegionName, handleCountryName }) => {
                 />
             </div>
 
-                <select name='option-region'
-                    aria-label='option-region'
-                    className='option-region'
-                    id='option-region'
-                    onChange={handleRegionName}  >
-                    <option hidden  >Filter By Region</option>
-                    <option hidden  >All Region</option>
-                    <option value="Americas">America</option>
-                    <option value="Africa">Africa</option>
-                    <option value="Oceania">Oceania</option>
-                    <option value="Asia">Asia</option>
-                    <option value="Europe">Europe</option>
-                </select>
+            <select name='option-region'
+                aria-label='option-region'
+                className='option-region'
+                id='option-region'
+                onChange={handleRegionName}  >
+                <option hidden  >Filter By Region</option>
+                <option value=""  >All Region</option>
+                <option value="Americas">America</option>
+                <option value="Africa">Africa</option>
+                <option value="Oceania">Oceania</option>
+                <option value="Asia">Asia</option>
+                <option value="Europe">Europe</option>
+            </select>
 
         </div>
     )
